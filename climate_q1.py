a10d78df83992844856da4568c805eb194ee256c
Dayyan <dayyan.ahmed@griffithuni.edu.au>
2023-09-14 13:22:22 +1000

    q1 updated

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,38 @@
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
 
-plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+# Connect to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
 
-plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+# Fetch data from the database and populate lists
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+Year, CO2, Temperature = zip(*data)  # Unzip the data into separate lists
+
+
+# Create a figure with two subplots (2 rows, 1 column)
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+# Plot CO2 data in the first subplot
+ax1.plot(Year, CO2, 'b--')
+ax1.set_title("Climate Data")
+ax1.set_ylabel("[CO2]")
+ax1.set_xlabel("Year (decade)")
+
+# Plot Temperature data in the second subplot
+ax2.plot(Year, Temperature, 'r*-')
+ax2.set_ylabel("Temp (C)")
+ax2.set_xlabel("Year (decade)")
+
+# Adjust spacing between subplots
+plt.tight_layout()
+
+# Show the plot
+plt.show()
+
+# Save the figure
+fig.savefig("co2_temp_1.png")
+
+# Close the database connection
+conn.close()
